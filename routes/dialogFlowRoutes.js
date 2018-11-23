2cc32fdc2df1f75709b699f6af4af3a1d0a2371a
Ravi <rickcarter1806@gmail.com>
2018-11-23 17:21:55 +0530

    added error handling

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -9,7 +9,11 @@ module.exports = app => {
           res.send(responses[0].queryResult);               
     });
     app.post('/api/df_event_query', async (req, res) => {
-          let responses = await chatbot.eventQuery(req.body.event, req.body.parameters);
-          res.send(responses[0].queryResult);   
+        try {
+            let responses = await chatbot.eventQuery(req.body.event, req.body.parameters);
+            res.send(responses[0].queryResult);     
+        } catch(e) {
+            res.send(e);     
+        }
     });
 }
