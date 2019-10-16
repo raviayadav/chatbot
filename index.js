f5e131bb7d39221ff891f951b5f2a2639bc602bd
Ravi <rickcarter1806@gmail.com>
2019-10-16 17:56:53 +0530

    relic added at top

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require('newrelic');
 const express = require('express');
 const app = express();
-require('newrelic');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json({limit: '50mb'}));
 // app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
